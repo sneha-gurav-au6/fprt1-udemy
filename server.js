1b391a35169329fe41e9e47ee4b8ca43330b532e
unknown <snehagurav14.sg@gmail.com>
2020-09-19 13:13:44 +0530

    m

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+
 require("./db");
 const PORT = process.env.PORT||5000
 
@@ -18,6 +19,11 @@ app.use(
 
 if(process.env.NODE_ENV==="production"){
     app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+
 }
 
 app.use(user);
